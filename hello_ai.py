a607b574e44bff1d63ae0df2271f8a6b7f2a6066
syyy78715-debug <syyy78715@gmail.com>
2026-08-18 20:23:39 +0800

    Update hello_ai.py
    Add Python for loop

--- hello_ai.py
+++ hello_ai.py
@@ -1,9 +1,4 @@
-password = ""
+names = ["小明", "小红", "小王", "商商"]
 
-while password != "123456":
-    password = input("请输入密码：")
-
-    if password != "123456":
-        print("密码错误，请重新输入。")
-
-print("密码正确，登录成功！")
+for name in names:
+    print("你好，", name)
