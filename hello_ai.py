2dc285af8b02d0fd0c7d91ac7340673f681420af
syyy78715-debug <syyy78715@gmail.com>
2026-08-18 14:06:10 +0800

    Update hello_ai.py
    Add Python conditional logic

--- hello_ai.py
+++ hello_ai.py
@@ -1,2 +1,8 @@
-print("Hello AI!")
-print("My first AI learning project.")
+name = input("请输入你的名字：")
+
+if name == "商商":
+    print("你好，商商！")
+    print("今天也继续学习 AI 吧！")
+else:
+    print("你好，", name)
+    print("欢迎来到 AI 学习之旅！")
