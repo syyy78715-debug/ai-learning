1c15c71e2bb4d2e59ee7aa1f0b627ea8866fcae6
syyy78715-debug <syyy78715@gmail.com>
2026-08-18 20:27:56 +0800

    Update hello_ai.py
    Add Python functions

--- hello_ai.py
+++ hello_ai.py
@@ -1,4 +1,8 @@
-names = ["小明", "小红", "小王", "商商"]
-
-for name in names:
+def say_hello(name):
     print("你好，", name)
+    print("欢迎来到 AI 学习之旅！")
+
+
+say_hello("商商")
+say_hello("小明")
+say_hello("小红")
