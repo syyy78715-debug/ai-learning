4d543a5d0ea437efa106f7a2320a40ff33bf68b1
syyy78715-debug <syyy78715@gmail.com>
2026-08-18 20:08:53 +0800

    Update hello_ai.py
    Add Python error handling

--- hello_ai.py
+++ hello_ai.py
@@ -1,7 +1,14 @@
-num1 = float(input("请输入第一个数字："))
-num2 = float(input("请输入第二个数字："))
+try:
+    num1 = float(input("请输入第一个数字："))
+    num2 = float(input("请输入第二个数字："))
 
-print("加法结果：", num1 + num2)
-print("减法结果：", num1 - num2)
-print("乘法结果：", num1 * num2)
-print("除法结果：", num1 / num2)
+    print("加法结果：", num1 + num2)
+    print("减法结果：", num1 - num2)
+    print("乘法结果：", num1 * num2)
+    print("除法结果：", num1 / num2)
+
+except ValueError:
+    print("输入错误：请输入数字。")
+
+except ZeroDivisionError:
+    print("计算错误：第二个数字不能是 0。")
