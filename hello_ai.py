f70be6dc89de66b9f7fe92cedaa12271ec689e3a
syyy78715-debug <syyy78715@gmail.com>
2026-08-18 21:39:42 +0800

    Protect API keys with gitignore

--- hello_ai.py
+++ hello_ai.py
@@ -1,11 +1,8 @@
-import requests
+from dotenv import load_dotenv
+import os
 
-response = requests.get("https://api.github.com")
+load_dotenv()
 
-print("状态码：", response.status_code)
+api_key = os.getenv("AI_API_KEY")
 
-data = response.json()
-
-print("GitHub API 返回的数据类型：", type(data))
-print("GitHub API 数据类型：", type(data))
-print("GitHub API 地址：", data["current_user_url"])
+print("API Key 已读取：", api_key)
